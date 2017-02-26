55d63f9f914b8b57c3cb0dbc2166448d8731b0f1
León Domingo <leon@codevince.com>
2017-02-26 18:25:45 +0100

    Fix installation of the package (pypandoc is not required)

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@
 from setuptools import setup
 import pypandoc
 
+try:
+    long_description = pypandoc.convert_file('./README.md', 'rst')
+
+except ImportError:
+    # when you're installing the package, to not force users to have "pypandoc" installed
+    with open('./README.md', 'r') as f:
+        long_description = f.read()
+
 setup(
     name='mydict',
-    version='1.0.4',
+    version='1.0.5',
     author=u'León Domingo',
     author_email='leon@codevince.dev',
     description='A Python dict subclass which tries to act like JavaScript objects, so you can use the dot-notation (d.foo) to access members of the object.',
-    long_description=pypandoc.convert_file('./README.md', 'rst'),
+    long_description=long_description,
     license='MIT',
     keywords='dict javascript dot-notation codevince',
     classifiers=[
