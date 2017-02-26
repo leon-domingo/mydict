bec0e7ddfc0fccda3e6e2b7f90971322e6de4bc4
León Domingo <leon@codevince.com>
2017-02-26 18:37:36 +0100

    Fix reference to "README.md" file (1.0.7)

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 # coding=utf8
 
 from setuptools import setup
+import os
 
+readme_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
 try:
     import pypandoc
-    long_description = pypandoc.convert_file('./README.md', 'rst')
+    long_description = pypandoc.convert_file(readme_path, 'rst')
 
 except ImportError:
-    # when you're installing the package, to not force users to have "pypandoc" installed
-    with open('./README.md', 'r') as f:
+    # when you're installing the package, "pypandoc" is not required
+    with open(readme_path) as f:
         long_description = f.read()
 
 setup(
     name='mydict',
-    version='1.0.6',
+    version='1.0.7',
     author=u'León Domingo',
     author_email='leon@codevince.dev',
     description='A Python dict subclass which tries to act like JavaScript objects, so you can use the dot-notation (d.foo) to access members of the object.',
