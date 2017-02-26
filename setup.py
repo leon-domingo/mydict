02b26db8e157ea66610a795870b987e742173072
León Domingo <leon@codevince.com>
2017-02-26 15:18:43 +0100

    Feed the "long_description" from README.md transformed into HTML ("markdown" package)

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 # coding=utf8
 
 from setuptools import setup
+import markdown
+
+# get HTML description from "README.md", transformed into HTML
+with open('./README.md', 'r') as readme_f:
+    html_desc = markdown.markdown(readme_f.read())
 
 setup(
     name='mydict',
-    version='1.0.2',
+    version='1.0.3',
     author=u'León Domingo',
     author_email='leon@codevince.dev',
-    description='A Python dict subclass which tries to act like JavaScript objects, so you can use the "dot notation" to access its members.',
+    description='A Python dict subclass which tries to act like JavaScript objects, so you can use the dot-notation (d.foo) to access members of the object.',
+    long_description=html_desc,
     license='MIT',
     keywords='dict javascript dot-notation codevince',
     classifiers=[
