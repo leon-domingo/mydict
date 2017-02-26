90094f82d7d6f0e74643004dc4673abb1e81acfe
León Domingo <leon@codevince.com>
2017-02-26 18:51:45 +0100

    Fix reference to "README.md" on package installation (once again...) (1.0.8)

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@
 from setuptools import setup
 import os
 
-readme_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+description = 'A Python dict subclass which tries to act like JavaScript objects, so you can use the dot-notation (d.foo) to access members of the object.',
+
 try:
     import pypandoc
+    readme_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
     long_description = pypandoc.convert_file(readme_path, 'rst')
 
 except ImportError:
-    # when you're installing the package, "pypandoc" is not required
-    with open(readme_path) as f:
-        long_description = f.read()
+    long_description = description
 
 setup(
     name='mydict',
-    version='1.0.7',
+    version='1.0.8',
     author=u'León Domingo',
     author_email='leon@codevince.dev',
-    description='A Python dict subclass which tries to act like JavaScript objects, so you can use the dot-notation (d.foo) to access members of the object.',
+    description=description,
     long_description=long_description,
     license='MIT',
     keywords='dict javascript dot-notation codevince',
